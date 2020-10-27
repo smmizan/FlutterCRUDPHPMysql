691b8032a10105dc1a867ff744af37413ce7e5d7
smmizancse <smmizancse@gmail.com>
2020-10-27 22:45:45 +0600

    home pages

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+  @override
+  _HomePagesState createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Pages'),
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+}
