48f62dae4165da5f6c910b5b8884bec90610b03a
smmizancse <smmizancse@gmail.com>
2020-10-27 22:57:07 +0600

    add model class

---
+++ lib/model/model.dart
@@ -0,0 +1,42 @@
+
+class Model{
+  String id;
+  String name;
+  String mail;
+  String address;
+
+  Model({
+    this.id,
+    this.name,
+    this.mail,
+    this.address
+  });
+
+
+  factory Model.fromJson(Map<String,dynamic> json){
+    return Model(
+      id: json['id'],
+      name: json['name'],
+      mail: json['mail'],
+      address: json['address'],
+    );
+
+  }
+
+
+
+  Map<String,dynamic> toJsonAdd() {
+    return{
+      "name":name,
+      "mail":mail,
+      "address":address,
+    };
+
+}
+
+
+
+
+
+
+}
